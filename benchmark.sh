70b23d7a82d9930b0d229ccc1c704dcec02b0f04
skwok1 <skwok1@mcn52.chicagobooth.edu>
2025-03-27 19:37:09 -0500

    try to run

---
+++ benchmark.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#SBATCH --account=pi-naragam
+#SBATCH --job-name=dcdi_native
+#SBATCH --output=./slurm/out/%j.%N.stdout
+#SBATCH --error=./slurm/out/%j.%N.stderr
+#SBATCH --chdir=/home/skwok1/dcdi
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=2000
+#SBATCH --time=3-00:00:00
+
+set -e
+
+module load python/booth/3.12
+
+for i in {1..10}; do
+    mkdir -p p10_e10_linear/dag_${i}
+    
+    LOG_FILE="slurm/out/dag_${i}.txt"
+
+    srun --exclusive --cpus-per-task=4 --ntasks=1 --nodes=1 bash -c "
+        python3 ./main.py \\
+            --train \\
+            --data-path ./data/perfect/data_p10_e10_n10000_linear_struct \\
+            --num-vars 10 \\
+            --i-dataset 10 \\
+            --exp-path p10_e10_linear/dag_${i} \\
+            --model DCDI-G \\
+            --reg-coeff 0.5 \\
+        | tee ${LOG_FILE}; tail -n 1000 ${LOG_FILE} > ${LOG_FILE}.tmp && mv ${LOG_FILE}.tmp ${LOG_FILE}
+    " &
+done
+
+wait

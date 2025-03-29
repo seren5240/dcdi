26da4985ecd9c2d823624c1f01db4d67f5d38619
skwok1 <skwok1@mfe02.chicagobooth.edu>
2025-03-29 09:56:09 -0500

    overwrite guardrail

--- benchmark.sh
+++ benchmark.sh
@@ -17,15 +17,23 @@ set -e
 module load python/booth/3.12
 module load R/4.3/4.3.2
 
+export R_LIBS_USER=~/Rlibs
+
+RESULTS_FOLDER="p10_e10_linear_noise_1"
+
+if [ -d "${RESULTS_FOLDER}" ]; then
+    echo "Results folder ${RESULTS_FOLDER} already exists, exiting."
+    exit 1
+fi
+
 for i in {1..10}; do
-    RESULTS_FOLDER="p10_e10_linear_dcd"
     LOG_FILE="${RESULTS_FOLDER}/dag_${i}/log.txt"
     mkdir -p ${RESULTS_FOLDER}/dag_${i}
 
     srun --exclusive --cpus-per-task=4 --ntasks=1 --nodes=1 bash -c "
         python3 ./main.py \\
             --train \\
-            --data-path ./data/perfect/data_p10_e10_n10000_linear_struct \\
+            --data-path ./data/ours/data_p10_e10.0_n1000_GP \\
             --num-vars 10 \\
             --i-dataset ${i} \\
             --exp-path ${RESULTS_FOLDER}/dag_${i} \\
